9f49acd4df3e0d9ec1d7cce8cd447e295dd600b4
Gabriel Duarte <gabrielduarte1405@gmail.com>
2023-06-19 17:00:36 -0300

    Requisito 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,26 @@
+from models.ingredient import Ingredient
+from models.dish import Dish
+import csv
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        with open(source_path, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for meal in reader:
+                meal_name = meal['dish']
+                meal_price = float(meal['price'])
+                meal_ingredients = meal['ingredient']
+                meal_amount = int(meal['recipe_amount'])
+
+                self.dishes.add(Dish(
+                    meal_name,
+                    meal_price,
+                    ))
+                prox_meal = next(iter(self.dishes))
+
+                prox_meal.add_ingredient_dependency(
+                    Ingredient(meal_ingredients), meal_amount,
+                )
