6f0636bdecbad2f5cb87033c01c7cf7e980915ec
Gabriel Duarte <gabrielduarte1405@gmail.com>
2023-06-19 20:03:40 -0300

    Requisito 4

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -7,20 +7,25 @@ import csv
 class MenuData:
     def __init__(self, source_path: str) -> None:
         self.dishes = set()
+        meals = {}
+
         with open(source_path, newline='') as csvfile:
             reader = csv.DictReader(csvfile)
             for meal in reader:
-                meal_name = meal['dish']
-                meal_price = float(meal['price'])
-                meal_ingredients = meal['ingredient']
-                meal_amount = int(meal['recipe_amount'])
+                meal_key = meal["ingredient"]
+                meal_ingredients = Ingredient(meal_key)
+                self.dishes.add(meal_ingredients)
+                meal_name = meal["dish"]
+                meal_price = float(meal["price"])
+                meal_atributes = meals.get(meal_name)
 
-                self.dishes.add(Dish(
-                    meal_name,
-                    meal_price,
-                    ))
-                prox_meal = next(iter(self.dishes))
+                if meal_atributes is None:
+                    meal_atributes = Dish(meal_name, meal_price)
+                    meals[meal_name] = meal_atributes
+                meal_amount = int(meal["recipe_amount"])
+                meal_atributes.add_ingredient_dependency(
+                    meal_ingredients,
+                    meal_amount,
+                    )
 
-                prox_meal.add_ingredient_dependency(
-                    Ingredient(meal_ingredients), meal_amount,
-                )
+        self.dishes = set(meals.values())
