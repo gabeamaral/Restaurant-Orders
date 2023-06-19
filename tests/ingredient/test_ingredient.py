5dfb158c66794cb977fa090e9874f1acdfc900b5
Gabriel Duarte <gabrielduarte1405@gmail.com>
2023-06-19 15:11:08 -0300

    Requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,23 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (Ingredient, Restriction)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    presunto = Ingredient("presunto")
+    assert presunto.name == "presunto"
+    assert presunto.restrictions == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.ANIMAL_DERIVED,
+    }
+
+    assert str(presunto) == "Ingredient('presunto')"
+
+    ingredient_1 = Ingredient("presunto")
+    ingredient_2 = Ingredient("presunto")
+    ingredient_3 = Ingredient("queijo mussarela")
+
+    assert ingredient_1 == ingredient_2
+    assert ingredient_1 != ingredient_3
+
+    assert hash(ingredient_1) == hash(ingredient_2)
+    assert hash(ingredient_1) != hash(ingredient_3)
